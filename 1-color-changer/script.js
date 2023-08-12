04ae1399925ebf9daac16ca7adc2a6b063788be2
Vishal Sharma <39647664+vsvishal@users.noreply.github.com>
2023-08-12 21:17:03 +0530

    project 1-color-changer completed

---
+++ 1-color-changer/script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach((button) => {
+  console.log(button);
+  button.addEventListener("click", (e) => {
+    console.log(e.target.id);
+      // if (e.target.id === 'grey') {
+      //   body.style.backgroundColor = e.target.id
+      // }
+      switch (e.target.id) {
+        case 'white': body.style.backgroundColor = e.target.id;
+                      body.style.color = "black";
+        break;
+        case 'grey': body.style.backgroundColor = e.target.id;
+        break;
+        case 'blue': body.style.backgroundColor = e.target.id;
+        break;
+        case 'yellow': body.style.backgroundColor = e.target.id;
+        break;
+      }
+  })
+})
